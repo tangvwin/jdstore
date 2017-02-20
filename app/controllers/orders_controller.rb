172043b4e054b8eae779b45ca2a6ac43c98ba052
Tony <tangvwin@gmail.com>
2017-02-20 23:10:49 +0800

    add product_lists model

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,5 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user! only: [:create]
+   before_action :authenticate_user!, only: [:create]
 
     def create
       @order = @order.new(order_params)
