512088e8c65882984e2fc2e5c93c6c1e5aa80033
Tony <tangvwin@gmail.com>
2017-02-21 15:37:47 +0800

    user can see their orders

---
+++ app/controllers/account/orders_controller.rb
@@ -0,0 +1,15 @@
+class Account::OrdersController < ApplicationController
+   before_action :authenticate_user!
+
+   def index
+     @orders = current_user.orders.order("id DESC")
+   end
+
+
+
+
+
+
+
+
+end
