8dfd49a2a8d1558d413bded25f7c556b5dc2c725
Tony <tangvwin@gmail.com>
2017-05-12 15:50:45 +0800

    implement product show

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,55 @@
+class ProductsController < ApplicationController
+
+
+   def index
+     @products = Product.all
+   end
+
+   def show
+     @product = Product.find(params[:id])
+   end
+
+   def edit
+     @product = Product.find(params[:id])
+   end
+
+   def new
+     @product = Product.new
+   end
+
+   def create
+     @product = Produt.new(product_params)
+
+     if @prodcut.save
+       redirect_to products_path
+     else
+       render :new
+     end
+   end
+
+
+   def update
+     @product = Product.find(params[:id])
+    if @product.update
+      redirect_to products_path
+    else
+      render :edit
+    end
+  end
+
+   def destroy
+     @product = Product.find(params[:id])
+     @prodcut.destroy
+     redirect_to prodcuts_path
+     flash[:alert] = "Product deleted!"
+   end
+
+   private
+
+   def product_params
+     params.require(:product).permit(:title, :description, :quantity, :price, :image)
+   end
+
+
+
+end
