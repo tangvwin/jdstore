19529bc2d61de5778c9ac92281a2aa25a6a58048
Tony <tangvwin@gmail.com>
2017-05-24 00:44:14 +0800

    upload the pic to qiuniu and change the url of pic

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,31 +18,31 @@ Product.create!(title: '华为 HUAWEI P10',
 description: '华为 HUAWEI P10 全网通 4GB+64GB 草木绿 移动联通电信4G手机 双卡双待',
 quantity: '200',
 price: '3788',
-image: open("https://img10.360buyimg.com/mobilecms/s500x500_jfs/t4735/313/2135200/301418/71e21085/58d8f5d5Na3023dcb.jpg!q70.jpg"))
+image: open("http://om2fym5z4.bkt.clouddn.com/huaweip10.jpg"))
 
 Product.create!(title: 'HTC U Ultra（U-1w）初绽（粉）',
 description: 'HTC U Ultra（U-1w）初绽（粉） 移动联通电信六模全网通 双卡双待双屏',
 quantity: '100',
 price: '5088',
-image: open("https://img11.360buyimg.com/mobilecms/s500x500_jfs/t3244/360/6728063286/236794/8c103f3c/58ae8d14N998fb78d.jpg!q70.jpg"))
+image: open("http://om2fym5z4.bkt.clouddn.com/htcu.jpg"))
 
 Product.create!(title: 'Meitu 美图T8',
 description: 'Meitu 美图T8（MP1602）4GB+128GB 魔力橙 自拍美颜 全网通 移动联通电信4G手机',
 quantity: '150',
 price: '4099',
-image: open("https://img12.360buyimg.com/mobilecms/s500x500_jfs/t3145/113/6389106578/178066/72950b8c/58a43d52Nd776bb35.jpg!q70.jpg"))
+image: open("http://om2fym5z4.bkt.clouddn.com/meitu.jpg"))
 
 Product.create!(title: 'vivo X9 全网通',
 description: 'vivo X9 全网通 4GB+64GB 活力蓝 移动联通电信4G手机 双卡双待',
 quantity: '120',
 price: '2798',
-image: open("https://img13.360buyimg.com/mobilecms/s500x500_jfs/t5431/160/1630814540/47653/2245d825/5912c06cN65c353ca.jpg!q70.jpg"))
+image: open("http://om2fym5z4.bkt.clouddn.com/vivox9.jpg"))
 
 Product.create!(title: 'Apple iPhone 7 Plus 128G 红色特别版',
 description: 'Apple iPhone 7 Plus 128G 红色特别版 移动联通电信4G手机',
 quantity: '50',
 price: '6699',
-image: open("https://img14.360buyimg.com/mobilecms/s500x500_jfs/t4276/257/2416766721/125228/ba72a107/58d1d078N20e18b62.jpg!q70.jpg"))
+image: open("http://om2fym5z4.bkt.clouddn.com/iphone7plus.jpg"))
 
 Product.create!(title: '小米MIX 全网通 标准版',
 description: '小米MIX 全网通 标准版 4GB内存 128GB ROM 皓月白 移动联通电信4G手机',
