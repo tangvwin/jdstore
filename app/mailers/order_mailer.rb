590582238adc29a3a29013dbb5ee2ec04954025c
Tony <tangvwin@gmail.com>
2017-05-25 21:02:05 +0800

    change the order_mailer.rb's mail to vwin@163.com

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -15,7 +15,7 @@ class OrderMailer < ApplicationMailer
       @user    = order.user
       @product_lists = @order.product_lists
 
-      mail(to: "admin@test.com" , subject: "[JDStore] 用户#{order.user.email}申请取消订单 #{order.token}")
+      mail(to: "vwin@163.com" , subject: "[JDStore] 用户#{order.user.email}申请取消订单 #{order.token}")
     end
 
     def notify_ship(order)
