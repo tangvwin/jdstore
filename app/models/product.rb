f4c1c528656aae085159b5425a4dc0f6a0134da0
Tony <tangvwin@gmail.com>
2017-05-27 10:35:54 +0800

    add comment function

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,9 @@
 class Product < ApplicationRecord
-
+has_many :comments
  mount_uploader :image, ImageUploader
 
 acts_as_list
 
- validates_numericality_of :discount, :greater_than => 0, :less_than => 100, :allow_blank => true 
+ validates_numericality_of :discount, :greater_than => 0, :less_than => 100, :allow_blank => true
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :comments
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
@@ -24,6 +25,7 @@ Rails.application.routes.draw do
 
 
   resources :products do
+    resources :comments
     collection do
       get :search
     end
