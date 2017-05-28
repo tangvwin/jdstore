5cd163da31a7800d2688a728547d96cfa3d82aa2
Tony <tangvwin@gmail.com>
2017-05-28 16:29:28 +0800

    fix the cart index's bug

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -24,7 +24,7 @@ class ImageUploader < CarrierWave::Uploader::Base
  process resize_to_fit: [800, 800]
 
  version :thumb do
-   process resize_to_fit: [200, 200]
+   process resize_to_fit: [100, 100]
  end
 
  version :medium do
