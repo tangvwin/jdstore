13f0fc9cb21eab890962894d78d96710cb5b49cf
Tony <tangvwin@gmail.com>
2017-06-07 13:40:55 +0800

    success upload the photo of user and edit it

--- app/controllers/account/users_controller.rb
+++ app/controllers/account/users_controller.rb
@@ -1,5 +1,34 @@
 class Account::UsersController < ApplicationController
 
+  def index
+
+    @users = User.all
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      render :edit
+    else
+    render :edit
+  end
+end
+
+  def create
+    @user = User.new
+    if @user.save
+      redirect_to account_users_path
+    else
+      render :new
+    end
+  end
+
+
+
   private
 
   def user_params
